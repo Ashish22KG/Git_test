8c0f80a5ac1a847ccedf50b34aa2851b86edddda
Ashish22KG <149960304+Ashish22KG@users.noreply.github.com>
2024-08-29 21:15:01 +0530

    design reset login

---
+++ reset-password.py
@@ -0,0 +1,8 @@
+username=input("enter name ")
+password= input("enter the password ")
+
+if username == " admin" and password == "admin":
+    print(" you are reset the password ")
+
+else :
+    print(" you cant reset the password ")
